216f1d1caf11755029a327786ede54305309b2fe
JT-Thrash <44144832+JT-Thrash@users.noreply.github.com>
2020-10-13 19:55:24 -0400

    Changes

--- Enemies/Rope/Rope.cs
+++ Enemies/Rope/Rope.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace Sprint2.Enemies.Rope
+namespace Sprint2
 {
     public class Rope
     {
+
+        private ISprite sprite;
+        private IEnemyState state;
+        private Vector2 location;
+        private Game game;
+
+
+        public Rope(Game game, Vector2 location)
+        {
+            this.game = game;
+            this.location = location;
+        }
+
+        public void Update()
+        {
+            state.Update();
+        }
+
+        public void Draw()
+        {
+            sprite.Draw()
+        }
     }
 }
