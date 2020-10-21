4ddebe0e9730bbc8ae8a1bf59338f26d33efdb17
HongYuan6139 <71283563+HongYuan6139@users.noreply.github.com>
2020-10-21 12:55:53 -0400

    Flame Collision

---
+++ Flame.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Sprint3
+{
+    /// <summary>
+    /// Author: Yuan Hong
+    /// </summary>
+    class Flame : INPC
+    {
+        private Vector2 flamePos;
+        private FlameSprite flameSprite;
+        private NPCCollider flameCollider;
+        public Flame(Vector2 initialPos, Texture2D texture)
+        {
+            flamePos = initialPos;
+            flameSprite =(FlameSprite) EnemySpriteFactory.Instance.CreateFlameSprite();
+            flameCollider =new NPCCollider(flameSprite.getRectangle(initialPos));
+        }
+
+        public void Update()
+        {
+            //do nothing
+        }
+        public void Draw(SpriteBatch spriteBatch, GameTime time)
+        {
+            flameSprite.Draw(spriteBatch, flamePos, 0, Color.White);
+        }
+
+        public NPCCollider GetCollider()
+        {
+            return flameCollider;
+        }
+    }
+}
---
+++ FlameSprite.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sprint3
+{
+    /// <summary>
+    /// Author: Yuan Hong
+    /// </summary>
+    class FlameSprite : ISprite
+    {
+        private Texture2D texture;
+        private static int[] SpriteSize = { 16, 16 };
+        private Vector2 sourcePos;
+        private const int NumUpdatePerSec = 30;
+        private const int FrameRate = 5;
+        private int numUpdatePerFrame = NumUpdatePerSec / FrameRate;
+        private int updateCounter;
+        private int frameIndex = 0;
+        private int spriteSizeIndex = 2;
+        private Point drawSize;
+            
+        public FlameSprite(Texture2D texture)
+        {
+            updateCounter = 0;
+            this.texture = texture;
+            sourcePos.X = EnemySpriteFactory.GetColumn("Flame") * SpriteSize[0];
+            sourcePos.Y = EnemySpriteFactory.GetRow("Flame") * SpriteSize[1];
+            drawSize.X = SpriteSize[0] * spriteSizeIndex;
+            drawSize.Y = SpriteSize[1] * spriteSizeIndex;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location, int currentFrame, Color color)
+        {
+            if(updateCounter == numUpdatePerFrame)
+            {
+                if(frameIndex == 0)
+                {
+                    frameIndex = 1;
+                }
+                else
+                {
+                    frameIndex = 0;
+                }
+                updateCounter = 0;
+                sourcePos.X = frameIndex * SpriteSize[0];
+            }
+            else
+            {
+                updateCounter++;
+            }
+            Rectangle sourceRectangle = new Rectangle((int)sourcePos.X, (int)sourcePos.Y, SpriteSize[0], SpriteSize[1]);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, drawSize.X, drawSize.Y);
+            spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
+        }
+
+        public Rectangle getRectangle(Vector2 location)
+        {
+            return new Rectangle((int)location.X, (int)location.Y, drawSize.X, drawSize.Y);
+        }
+    }
+}
