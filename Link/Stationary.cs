c636e2f6e69382ba0ae62750652e221622b713a4
Krescenskok <71037216+Krescenskok@users.noreply.github.com>
2020-10-22 13:16:43 -0400

    Update Stationary.cs

--- Link/Stationary.cs
+++ Link/Stationary.cs
@@ -6,7 +6,9 @@ namespace Sprint3.Link
 {
     public class Stationary : Movement
     {
-        
+
+        private double lastTime;
+
         public Stationary(LinkPlayer link) : base(link)
         {
 
@@ -26,7 +28,7 @@ namespace Sprint3.Link
                 {
                     Color col = colors[i];
                     i++;
-                    linkSprite.Draw(spriteBatch, location, 96, col);
+                    linkSprite.Draw(spriteBatch, location, currentFrame, col);
 
                     if (i == colors.Length - 1)
                     {
@@ -45,36 +47,109 @@ namespace Sprint3.Link
                 {
                     linkSprite = SpriteFactory.Instance.CreateLinkSprite();
                 }
-                linkSprite.Draw(spriteBatch, location, 96, Color.White);
+                linkSprite.Draw(spriteBatch, location, currentFrame, Color.White);
             }
 
         }
 
-        public override Vector2 HandleMagicalRod(GameTime gameTime,Vector2 location)
+        public override Vector2 HandleWoodenSword(GameTime gameTime, Vector2 location)
         {
-            throw new NotImplementedException();
-        }
 
-        public override Vector2 HandleShield(GameTime gameTime,Vector2 location)
-        {
-            throw new NotImplementedException();
-        }
+            if (gameTime.TotalGameTime.TotalMilliseconds - lastTime > 100)
+            {
+                lastTime = gameTime.TotalGameTime.TotalMilliseconds;
 
-        public override Vector2 HandleSword(GameTime gameTime,Vector2 location)
-        {
-            throw new NotImplementedException();
-        }
+                switch (currentFrame)
+                {
+                    case 0:
+                    case 1: currentFrame = 23; break;
+                    case 23: currentFrame = 22; break;
+                    case 22: currentFrame = 21; break;
+                    case 21: currentFrame = 20; break;
+                    case 20:
+                        currentFrame = 1;
+                        link.IsAttacking = false;
+                        link.IsStopped = true;
+                        break;
+                }
+            }
 
-        public override Vector2 HandleWoodenSword(GameTime gameTime,Vector2 location)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Vector2 Update(GameTime gameTime,Vector2 location)
-        {
             return location;
         }
 
-        
+        public override Vector2 HandleSword(GameTime gameTime, Vector2 location)
+        {
+            if (gameTime.TotalGameTime.TotalMilliseconds - lastTime > 100)
+            {
+                lastTime = gameTime.TotalGameTime.TotalMilliseconds;
+
+                switch (currentFrame)
+                {
+                    case 0:
+                    case 1: currentFrame = 45; break;
+                    case 45: currentFrame = 44; break;
+                    case 44: currentFrame = 43; break;
+                    case 43: currentFrame = 42; break;
+                    case 42:
+                        currentFrame = 1;
+                        link.IsAttacking = false;
+                        link.IsStopped = true;
+                        break;
+                }
+            }
+
+            return location;
+        }
+
+
+        public override Vector2 HandleMagicalRod(GameTime gameTime, Vector2 location)
+        {
+            if (gameTime.TotalGameTime.TotalMilliseconds - lastTime > 100)
+            {
+                lastTime = gameTime.TotalGameTime.TotalMilliseconds;
+
+                switch (currentFrame)
+                {
+                    case 0:
+                    case 1: currentFrame = 77; break;
+                    case 77: currentFrame = 76; break;
+                    case 76: currentFrame = 75; break;
+                    case 75: currentFrame = 74; break;
+                    case 74:
+                        currentFrame = 1;
+                        link.IsAttacking = false;
+                        link.IsStopped = true;
+                        break;
+                }
+            }
+
+            return location;
+        }
+
+        public override Vector2 HandleShield(GameTime gameTime, Vector2 location)
+        {
+            if (gameTime.TotalGameTime.TotalMilliseconds - lastTime > 100)
+            {
+                
+                lastTime = gameTime.TotalGameTime.TotalMilliseconds;
+
+                if (currentFrame == 0)
+                {
+                    currentFrame = 1;
+                }
+                else
+                    currentFrame = 0;
+                
+            }
+            link.IsAttacking = false;
+            link.IsStopped = true;
+
+            return location;
+        }
+
+        public override Vector2 HandlePickUpItem(GameTime gameTime, Vector2 location)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
