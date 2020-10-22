5bd64e1efc30c39f2e5effb8c81c84302e02d173
HongYuan6139 <71283563+HongYuan6139@users.noreply.github.com>
2020-10-22 11:08:30 -0400

    Add files via upload

--- NPCCollider.cs
+++ NPCCollider.cs
@@ -7,38 +7,43 @@ namespace Sprint3
 {
     public class NPCCollider : ICollider
     {
+        private Rectangle bounds;
+
         public NPCCollider(Rectangle rectangle)
         {
-
+            bounds = rectangle;
+            CollisionHandler.Instance.AddCollider(this);
         }
+
         public Rectangle Bounds()
         {
-            throw new NotImplementedException();
+            return bounds;
         }
 
         public bool CompareTag(string tag)
         {
-            throw new NotImplementedException();
+            return tag == "NPC";
         }
 
         public bool Equals(ICollider col)
         {
-            throw new NotImplementedException();
+            return this == col;
         }
 
         public void HandleCollision(ICollider col, Collision collision)
         {
-            throw new NotImplementedException();
         }
 
         public void HandleCollisionEnter(ICollider col, Collision collision)
         {
-            throw new NotImplementedException();
         }
 
         public void SendMessage(string msg, object value)
         {
-            throw new NotImplementedException();
+        }
+        public void Update(Point point)
+        {
+            bounds.Location = point;
         }
     }
 }
