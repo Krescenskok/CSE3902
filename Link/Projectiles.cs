1c066b5c5d2d6e603584bbc48b90f1029b651fd2
Krescenskok <71037216+Krescenskok@users.noreply.github.com>
2020-10-23 21:16:21 -0400

    Add files via upload

---
+++ Link/Projectiles.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Sprint3.Link
+{
+    public class Projectiles
+    {
+
+        LinkPlayer link;
+        Vector2 itemLocation;
+  
+
+        public static Projectiles Instance
+        {
+            get { return Instance; }
+        }
+
+        public Projectiles(LinkPlayer link)
+        {
+            this.link = link;
+            itemLocation = link.currentLocation;
+
+            if (link.state is MoveRight)
+            {
+                itemLocation.X += 10;
+            }
+            else if (link.state is MoveLeft)
+            {
+                itemLocation.X -= 10;
+            }
+            else if (link.state is MoveUp)
+            {
+                itemLocation.Y -= 10;
+            }
+            else if (link.state is MoveDown)
+            {
+                itemLocation.Y += 10;
+            }
+        }
+
+        public void ArrowBow()
+        {
+
+        }
+
+        public void SwordBeam()
+        {
+          
+        }
+
+    }
+}
