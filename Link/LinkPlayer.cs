7a489b8132fc5518ccea398e0ec9bd9c6093b578
Krescenskok <71037216+Krescenskok@users.noreply.github.com>
2020-11-16 14:35:44 -0500

    Update LinkPlayer.cs

--- Link/LinkPlayer.cs
+++ Link/LinkPlayer.cs
@@ -131,7 +131,7 @@ namespace Sprint4
         }
 
 
-        private ItemForLink currentWeapon = ItemForLink.WoodenSword;
+        private ItemForLink currentWeapon = ItemForLink.Shield;
 
 
         public ItemForLink CurrentWeapon
