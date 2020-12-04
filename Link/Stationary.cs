8f9cea119f542e7ab7f28be4b5ac23855fb91365
Krescens Kok <71037216+Krescenskok@users.noreply.github.com>
2020-12-03 21:31:53 -0500

    fix text

--- Link/Stationary.cs
+++ Link/Stationary.cs
@@ -88,7 +88,7 @@ namespace Sprint5
 
         public override Vector2 HandlePickUpItem(GameTime gameTime, Vector2 location)
         {
-            throw new NotImplementedException();
+            return location;
         }
 
         
